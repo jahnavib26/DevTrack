086df023b0a72b4bbd30def85621cb908bc86463
Jahnavi Bollineni <jahnavibollineni@mac.notrust.clients.wireless.pfw.edu>
2024-11-18 20:55:17 -0500

    Create Project, Dashboard Created

--- devtrack_frontend/src/App.js
+++ devtrack_frontend/src/App.js
@@ -3,14 +3,22 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
