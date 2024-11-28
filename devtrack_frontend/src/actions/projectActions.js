c1b5fb22ffcf8b85b2aa539556296e1f4e41b5e7
Adithya SN <adithyasn7gmail.com@Adithyas-MacBook-Air.local>
2024-11-28 13:37:53 -0500

    update PROPS

--- devtrack_frontend/src/actions/projectActions.js
+++ devtrack_frontend/src/actions/projectActions.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
+
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
@@ -10,4 +11,12 @@ export const createProject = (project, history) => async dispatch => {
       payload: err.response.data
     });
   }
+};
+
+export const getProjects = () => async dispatch => {
+  const res = await axios.get("http://localhost:8080/api/project/all");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data
+  });
 };
