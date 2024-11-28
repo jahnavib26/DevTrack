5ff7d3d692eb1e429e6b91303ed0229fba5b72e4
Adithya SN <adithyasn7gmail.com@Adithyas-MacBook-Air.local>
2024-11-28 12:36:28 -0500

    adding redux extension component

--- devtrack_frontend/src/App.js
+++ devtrack_frontend/src/App.js
@@ -3,14 +3,25 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+      </Provider>
     );
   }
 }
